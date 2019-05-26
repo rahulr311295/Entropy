01d96ee930d339e762697935c1c2b789e8a61a6e
root <root@localhost.localdomain>
2019-05-26 21:59:46 +0530

    Initial Commit

---
+++ entropy.py
@@ -0,0 +1,7 @@
+import jsbeautifier
+# res = jsbeautifier.beautify('your javascript string')
+opts = jsbeautifier.default_options()
+opts.indent_size = 2
+opts.space_in_empty_paren = True
+res = jsbeautifier.beautify_file('some_file.js',opts)
+print(res)
