ae38abb830c9ea1d03dafe68b5037f9ca840ff5b
Mufeed VH <mufeedvh@gmail.com>
2019-10-05 01:08:13 +0530

    Added new features & cleaned the code
    ADDED A FEATURE: Input the JavaScript file with arguments or CLI input.
    And, cleaned the code...

--- entropy.py
+++ entropy.py
@@ -1,7 +1,30 @@
 import jsbeautifier
 import requests
+import sys
+
 opts = jsbeautifier.default_options()
 opts.indent_size = 2
 opts.space_in_empty_paren = True
-res = jsbeautifier.beautify_file('some_file.js',opts)
-print(res)
+
+def beautify(file_to_beautify):
+	res = jsbeautifier.beautify_file(file_to_beautify, opts)
+	print(res)
+	print("\n\nSaving beautified JavaScript code...")
+	open('beautified_code.js', 'w+').write(res)
+	print("\nFile saved as beautified_code.js")
+
+def main():
+	try:
+		arg = sys.argv[1]
+		if arg == 'help':
+			print("\nUsage: python entropy.py javascript_file.js\n")
+		else:
+			file_to_beautify = str(sys.argv[1])
+			beautify(file_to_beautify)
+	except:
+		print("\nYou can also use: python entropy.py javascript_file.js\n")
+		file_to_beautify = raw_input("Enter the name fo the JavaScript file you want to Beautify: ")
+		beautify(file_to_beautify)
+
+if __name__ == '__main__':
+	main()
